11e347dc6d9473a1387a359dbebfc3356564ae99
PuLin <1454655719@qq.com>
2025-08-21 21:51:31 +0800

    DASH-Admin快速开发基础框架0.0.2

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ if __name__ == "__main__":
     # 生产环境推荐使用gunicorn启动
     dash_logger.warning("系统启动完成...",logmodule=dash_logger.logmodule.SYSTEM,operation=dash_logger.operation.SYSTEM_START)
     app.run(debug=True) 
+    
 
 
 
